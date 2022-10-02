777244428b5045f62db358870df9f251a798082c
Sorasouki <58504567+Sorasouki@users.noreply.github.com>
2022-10-02 18:43:35 +0700

    Add files via upload

---
+++ knn.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+#read in the data using pandas
+df = pd.read_csv('iris.csv')
+pd.set_option('display.max_columns', None)
+#check data has been read in properly
+df.head()
+#check number of rows and columns in dataset
+df.shape
+#create a dataframe with all training data except the target column
+X = df.drop(columns=['Label'])
+#check that the target variable has been removed
+X.head()
+#separate target values
+y = df['Label'].values
+#view target values
+y[0:5]
+
+
+#split dataset into train and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
+# Create KNN classifier
+knn = KNeighborsClassifier(n_neighbors = 5)
+# Fit the classifier to the data
+knn.fit(X_train,y_train)
+#show first 5 model predictions on the test data
+knn.predict(X_test)[0:5]
+#check accuracy of our model on the test data
+acc = (knn.score(X_test, y_test))
+print('Accuracy k-5 =', '%.2f'%(acc*100), '%')
---
+++ knnLOO.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+from numpy import mean
+from numpy import sqrt
+from numpy import absolute
+
+#read in the data using pandas
+df = pd.read_csv('iris.csv')
+pd.set_option('display.max_columns', None)
+#check data has been read in properly
+df.head()
+#check number of rows and columns in dataset
+df.shape
+#create a dataframe with all training data except the target column
+X = df.drop(columns=['Label'])
+#check that the target variable has been removed
+X.head()
+#separate target values
+y = df['Label'].values
+#view target values
+y[0:5]
+
+#create a new KNN model
+knn_cv = KNeighborsClassifier(n_neighbors=5)
+#train model with cv of 5 
+cv_scores = cross_val_score(knn_cv, X, y, cv=50)
+mean_cv_scores = mean(absolute(cv_scores))
+error_cv_scores = sqrt(mean(absolute(cv_scores)))
+#print each cv score (accuracy) and average them
+print(cv_scores)
+print('cv_scores mean =', '%.2f'%(mean_cv_scores*100), '%')
+print('cv_scores Error mean =', '%.2f'%(100-(mean_cv_scores*100)), '%')
+print('cv_scores True Error mean =', '%.2f'%(100-(error_cv_scores*100)), '%')
