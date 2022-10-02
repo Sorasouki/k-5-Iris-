4beae126a07abd26008c4203869f4cccde59f666
Sorasouki <58504567+Sorasouki@users.noreply.github.com>
2022-10-02 20:05:56 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-
+from numpy import sqrt, mean, absolute
 #read in the data using pandas
 df = pd.read_csv('https://github.com/achmatim/data-mining/blob/main/Dataset/iris.csv?raw=true')
 pd.set_option('display.max_columns', None)
@@ -18,7 +18,6 @@ y = df['Label'].values
 #view target values
 y[0:5]
 
-
 #split dataset into train and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
 # Create KNN classifier
@@ -29,4 +28,7 @@ knn.fit(X_train,y_train)
 knn.predict(X_test)[0:5]
 #check accuracy of our model on the test data
 acc = (knn.score(X_test, y_test))
-print('Accuracy k-5 =', '%.2f'%(acc*100), '%')
+error_acc = sqrt(absolute(acc))
+print('Accuracy =', '%.2f'%(acc*100), '%')
+print('Error =', '%.2f'%(100-(acc*100)), '%')
+print('True Error Probability =', '%.2f'%(100-(error_acc*100)), '%')
